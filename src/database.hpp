e372e301598df98e6b202e62a3fd5c84dedcd74d
Arki <gameak8800@gmail.com>
2025-03-18 10:29:59 +0300

    Update database.hpp

--- src/database.hpp
+++ src/database.hpp
@@ -2,38 +2,38 @@
 #define DATABASE_HPP_
 
 #include <map>
+#include <optional>
 #include <sstream>
 #include <string>
 #include <vector>
-#include "db_entry.hpp"
 #include "sstable.hpp"
 
 namespace DB {
 
 class Database {
- private:
-  std::map<std::string, DBEntry> memtable;
-  std::vector<SSTable> sstables;
-  size_t memtableLimit;
-  size_t sstableLimit;
-  std::string directory;
+private:
+    std::map<std::string, std::optional<std::string>> memtable;
+    std::vector<SSTable> sstables;
+    size_t memtableLimit;
+    size_t sstableLimit;
+    std::string directory;
 
-  void flushMemtable();
-  void mergeSSTables();
+    void flushMemtable();
+    void compactSSTables();
+    void mergeSSTables();
 
- public:
+public:
+    Database(const std::string &directory, size_t memLimit, size_t sstLimit);
 
-  Database(const std::string& directory, size_t memLimit, size_t sstLimit);
+    void insert(const std::string &key, const std::string &value);
 
-  void insert(const std::string& key, const std::string& value);
+    bool remove(const std::string &key);
 
-  bool remove(const std::string& key);
+    std::optional<std::string> select(const std::string &key);
 
-  std::optional<std::string> select(const std::string& key);
+    void flush();
 
-  void flush();
-
-  void merge();
+    void merge();
 };
 
 }  // namespace DB
