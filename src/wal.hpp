53449c49926cdf86eab84e1e1e4c1e0cdd299851
Arki <gameak8800@gmail.com>
2025-04-14 20:52:03 +0300

    Update wal.hpp

--- src/wal.hpp
+++ src/wal.hpp
@@ -1,33 +1,31 @@
 #ifndef WAL_HPP_
 #define WAL_HPP_
 
-#include <boost/iostreams/device/file_descriptor.hpp>
-#include <boost/iostreams/stream.hpp>
+#include <fstream>
 #include <functional>
 #include <string>
 #include <userver/engine/mutex.hpp>
-
 namespace DB {
 
 class WAL {
-public:
-    WAL(const std::string &filename);
-    ~WAL();
+ public:
+  WAL(const std::string& filename);
+  ~WAL();
 
-    void logInsert(const std::string &key, const std::string &value);
-    void logRemove(const std::string &key);
 
-    void recover(
-        std::function<void(const std::string &, const std::string &, bool)>
-            applyOperation
-    );
+  void logInsert(const std::string& key, const std::string& value);
+  void logRemove(const std::string& key);
 
-    void clear();
 
-private:
-    std::string filename_;
-    int fd_out_;
-    userver::engine::Mutex walMutex_;
+  void recover(std::function<void(const std::string&, const std::string&, bool)>
+                   applyOperation);
+
+  void clear();
+
+ private:
+  std::string filename_;
+  std::ofstream ofs_;
+  userver::engine::Mutex walMutex_;
 };
 
 }  // namespace DB
